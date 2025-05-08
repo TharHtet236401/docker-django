989eab76c71a8f9b8627acfbda3dfbb08342e6f7
TharHtet236401 <tharhteta3@gmail.com>
2025-05-08 23:38:27 +0700

    after adding static

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './booksManagement/templates/booksManagement/**/*.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
